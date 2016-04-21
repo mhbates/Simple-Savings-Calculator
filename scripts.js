2f0965e9b687d7ded7e6e047ec76909e8716eeb4
Matthew Hathaway-Bates <matt@mhbates.com>
2016-04-20 23:11:29 -0700

    Interest feature and bugfixes
    HTML:
    add links to source and mhbates.com
    remove caption all boxes required
    
    CSS:
    shift content up a bit
    add overflow-x hidden to body to fix big x margin on mobile devices
    change header and source code to em font sizes (vw unit shrinks too much)
    add margin between buttons
    
    JS:
    add Interest Gained feature (new balance minus old balance)

--- scripts.js
+++ scripts.js
@@ -24,36 +24,54 @@ function calc() {
     // convert interest rate % into decimal form
     var interestDecimal = interestRate/100;
 
-    // create html table and append it to the div container
+    // create blank html table and append it to the div container
     var createTable = document.createElement("table");
     createTable.id = "createTable";
     centerDiv.appendChild(createTable);
 
+    // adds header elements to the table (Year, New Balance, etc)
     var resultHead = createTable.createTHead();
     var headRow = resultHead.insertRow();
     var headerCell0 = document.createElement("th");
     var headerCell1 = document.createElement("th");
+    var headerCell2 = document.createElement("th");
     headerCell0.innerHTML = "Year";
     headRow.appendChild(headerCell0);
     headerCell1.innerHTML = "New Balance";
     headRow.appendChild(headerCell1);
+    headerCell2.innerHTML = "Interest";
+    headRow.appendChild(headerCell2);
 
     // run calculation and fill table rows until the # of years entered is reached
     for (var yearCount = 0; yearCount <= years; ++yearCount)
     {
+        // var used in yearly change calculation a few lines below (eg Year 2 Balance minus Year 1 Balance)
+        var oldBalance = newBalance 
+
+        // calculate new balance (principal * (interest rate to the power of yearCount))
         var newBalance = principal * Math.pow(1 + interestDecimal, yearCount);
 
+        // calculates yearly change (eg Year 2 Balance minus Year 1 Balance)
+        var amountChange = newBalance - oldBalance;
+
+        // adds row and cells to the table (for current year)
         var resultTable = document.getElementById("createTable");
-
         var tableRow = resultTable.insertRow(1 + yearCount);
-
         var cell0 = tableRow.insertCell(0);
         var cell1 = tableRow.insertCell(1);
+        var cell2 = tableRow.insertCell(2);
 
+        // fills table cells with appropriate numbers (what year it is, what the new balance is, etc)
         cell0.innerHTML = yearCount;
         cell1.innerHTML = "$" + newBalance.toFixed(2);
 
-
+        // if yearly change value is not a number, fill the cell with $0.00 (this fixes Year 0 yearly change weirdness)
+        // else, if the yearly change value IS a number, insert the appropriate value
+        if (isNaN(amountChange)) {
+            cell2.innerHTML = "$0.00";
+        } else  {
+            cell2.innerHTML = "$" + amountChange.toFixed(2);
+        }
     }
 }
 
