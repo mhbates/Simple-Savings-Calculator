4d933a1b588e1e175684d2cfb6bf1624f6cc6f31
Matthew Hathaway-Bates <matt@mhbates.com>
2016-04-22 10:38:41 -0700

    Simple Compound Rate Feature
    pretty limited
    for now, if daily compound rate is selected, the time unit also changes to daily
    so if I wanted to calculate a year's worth of daily-compounded interest, I would have to enter 365 in the timeframe input box
    not feasible for long-term calculations since the script would hang (20 years=7300 days)
    
    will probably remove daily rate in next version since it doesn't really apply to savings/retirement accounts anyway. will put back in once student loan feature is ready

--- scripts.js
+++ scripts.js
@@ -1,6 +1,25 @@
 // init this variable first because functions below depend on it
 var centerDiv = document.getElementById("div1");
 
+// function to change 'timeframe' input placeholder text to match Compound Rate radio selection
+var currentCompoundRate;
+function compoundDaily()  {
+    currentCompoundRate = document.getElementById("dailyCompound").value;
+    document.getElementById("timeframe").placeholder = "Enter # of " + currentCompoundRate + "s here";
+}
+
+function compoundMonthly()  {
+    currentCompoundRate = document.getElementById("monthlyCompound").value;
+    document.getElementById("timeframe").placeholder = "Enter # of " + currentCompoundRate + "s here";
+}
+
+function compoundYearly()  {
+    currentCompoundRate = document.getElementById("yearlyCompound").value;
+    document.getElementById("timeframe").placeholder = "Enter # of " + currentCompoundRate + "s here";
+}
+
+
+
 // runs the calculation and generates results table
 function calc() {
 
@@ -13,13 +32,13 @@ function calc() {
 
     // init variables for input and convert them to floating numbers or integers
     var principal = document.getElementById("principal").value;
-    principal = parseFloat(principal); // doing this because type=number is still considered a string and not a number
+    principal = parseFloat(principal);
 
     var interestRate = document.getElementById("interestRate").value;
     interestRate = parseFloat(interestRate);
 
-    var years = document.getElementById("years").value;
-    years = parseInt(years);
+    var timeframe = document.getElementById("timeframe").value;
+    timeframe = parseInt(timeframe);
 
     // convert interest rate % into decimal form
     var interestDecimal = interestRate/100;
@@ -35,34 +54,42 @@ function calc() {
     var headerCell0 = document.createElement("th");
     var headerCell1 = document.createElement("th");
     var headerCell2 = document.createElement("th");
-    headerCell0.innerHTML = "Year";
+    headerCell0.innerHTML = currentCompoundRate;
     headRow.appendChild(headerCell0);
     headerCell1.innerHTML = "New Balance";
     headRow.appendChild(headerCell1);
     headerCell2.innerHTML = "Interest";
     headRow.appendChild(headerCell2);
 
-    // run calculation and fill table rows until the # of years entered is reached
-    for (var yearCount = 0; yearCount <= years; ++yearCount)
+    // run calculation and fill table rows until the # of timeframe entered is reached
+    for (var timeCount = 0; timeCount <= timeframe; ++timeCount)
     {
         // var used in yearly change calculation a few lines below (eg Year 2 Balance minus Year 1 Balance)
         var oldBalance = newBalance 
 
-        // calculate new balance (principal * (interest rate to the power of yearCount))
-        var newBalance = principal * Math.pow(1 + interestDecimal, yearCount);
+        // calculate new balance (principal * (interest rate to the power of timeCount))
+        if (currentCompoundRate == "Day") {
+            var newBalance = principal * Math.pow(1 + (interestDecimal/365), timeCount);
+        }
+        else if (currentCompoundRate == "Month") {
+            var newBalance = principal * Math.pow(1 + (interestDecimal/12), timeCount);
+        }
+        else if (currentCompoundRate == "Year") {
+            var newBalance = principal * Math.pow(1 + interestDecimal, timeCount);
+        }
 
         // calculates yearly change (eg Year 2 Balance minus Year 1 Balance)
         var amountChange = newBalance - oldBalance;
 
         // adds row and cells to the table (for current year)
         var resultTable = document.getElementById("createTable");
-        var tableRow = resultTable.insertRow(1 + yearCount);
+        var tableRow = resultTable.insertRow(1 + timeCount);
         var cell0 = tableRow.insertCell(0);
         var cell1 = tableRow.insertCell(1);
         var cell2 = tableRow.insertCell(2);
 
         // fills table cells with appropriate numbers (what year it is, what the new balance is, etc)
-        cell0.innerHTML = yearCount;
+        cell0.innerHTML = timeCount;
         cell1.innerHTML = "$" + newBalance.toFixed(2);
 
         // if yearly change value is not a number, fill the cell with $0.00 (this fixes Year 0 yearly change weirdness)
@@ -75,8 +102,10 @@ function calc() {
     }
 }
 
+
 // Clear button also clears table from last calculation
 function clearTable() {
+    document.getElementById("timeframe").placeholder = "Select compound rate";
     if (document.getElementById("createTable"))
     {
         var removeTable = document.getElementById("createTable");
